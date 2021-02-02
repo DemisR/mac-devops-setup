17e8ef15c6e91f0403aebc65135a5cd78c2d8291
Demis Rizzotto <demis.r@me.com>
2021-02-02 22:48:40 +0100

    Install on Mac OS Big Sur

--- Install.sh
+++ Install.sh
@@ -1,12 +1,22 @@
+# Installing Xcode Command Line Tools
+xcode-select --install
+
+# install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew doctor
+
+# Install python3
+brew install python
+echo 'export PATH="/usr/local/opt/python/libexec/bin:$PATH"' >> ~/.zshrc
+export PATH="/usr/local/opt/python/libexec/bin:$PATH"
 
 # Install PIP
-sudo easy_install pip
+# curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+# sudo python get-pip.py
 
 # Install Ansible
 sudo pip install ansible
-
-# Installing Xcode Command Line Tools
-xcode-select --install
+brew install git
 
 # Clone this repository to your local drive
 git clone https://github.com/DemisR/mac-devops-setup.git
@@ -15,4 +25,6 @@ git clone https://github.com/DemisR/mac-devops-setup.git
 
 # Run `ansible-playbook setup-my-mac.yml -i inventory -K` inside this directory. Enter your account password when prompted.
 cd mac-devops-setup
+ansible-galaxy collection install community.general
+
 ansible-playbook setup-my-mac.yml -i inventory -K
